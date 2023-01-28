d3a0938a0abb74f3db0a15c037b8e568518f8ffa
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2023-01-28 14:19:10 -0300

    A simple code in java example

---
+++ list-java-01/helloworld.java
@@ -0,0 +1,13 @@
+//Welcome learning Java programming language
+
+
+public class helloworld {
+    //Class is a blueprint for creating objects
+
+    public static void main(String[] args) {
+        // main method is the entry point of the program
+
+        String name = "Hello World"; // String is a clas, type of variable
+        System.out.println(name); // print out the string 
+    }
+}
